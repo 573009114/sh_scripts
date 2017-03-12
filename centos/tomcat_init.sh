a99ecd59c979cc573f82dcb4caabee409f35b8b2
youzibaby <564793567@qq.com>
2017-03-12 09:37:21 +0800

    update

--- centos/tomcat_init.sh
+++ centos/tomcat_init.sh
@@ -4,15 +4,9 @@ cp -rf ./* /home
 cd /home 
 rpm -ivh *.rpm 
 cat>>/etc/profile<<EOF
-JAVA_HOME=/usr/java/jdk1.8.0_121
-JRE_HOME=$JAVA_HOME/jre
-PATH=$PATH:$JAVA_HOME/bin
-CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
-export JAVA_HOME
-export JRE_HOME
-export PATH
-export CLASSPATH
+export JAVA_HOME JAVA_HOME=/usr/java/jdk1.8.0_121
 EOF
 source /etc/profile
 #tomcat
 tar -xvf tomcat8.tar 
+  
