9518f6b97eb17b5ba30d7ba3315fd89c7353e15d
youzibaby <564793567@qq.com>
2017-03-13 17:11:15 +0800

    update

--- centos/1_net_init.sh
+++ centos/1_net_init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\033[32m 正在自动化配置系统环境...... \033[0m"
+echo -e "\033[32m 开始配置系统环境...... \033[0m"
 sleep 3s
 ##网卡自动化配置
 read -p "输入主机名:" HOSTNAME
@@ -8,14 +8,16 @@ read -p "输入子网掩码:" MS
 read -p "输入网关地址：" GT
 hostnamectl set-hostname $HOSTNAME
 cd /etc/sysconfig/network-scripts/
-net=ifcfg-eth0
+net=$(ls|grep 'ifcfg-em1')
 sed -i s/dhcp/static/g /etc/sysconfig/network-scripts/$net
+sed -i s/ONBOOT=no/ONBOOT=yes/g /etc/sysconfig/network-scripts/$net
 cat>>/etc/sysconfig/network-scripts/$net<<EOF
 IPADDR=$IP
 NETMASK=$MS
 GATEWAY=$GT
-DNS1=114.114.114.114
+DNS1=223.5.5.5
+DNS2=223.6.6.6
 EOF
-echo -e "\033[32m 网卡配置完毕,正在重启... \033[0m"
-sleep 3s
-service network restart
+sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config
+setenforce 0
+echo -e "\033[32m 网卡配置完毕. \033[0m"
